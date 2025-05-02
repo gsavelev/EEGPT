beb0d7689e906cd0d602eebb329d8ecd363b4240
gsavelev <super.gleb@pm.me>
2025-05-02 10:34:23 +0300

    try to fix deminsion error

--- downstream/Modules/models/EEGPT_mcae_finetune.py
+++ downstream/Modules/models/EEGPT_mcae_finetune.py
@@ -581,7 +581,9 @@ class EEGTransformer(nn.Module):
             ch = ch.upper().strip('.')
             assert ch in CHANNEL_DICT, ch
             chan_ids.append(CHANNEL_DICT[ch])
-        return torch.tensor(chan_ids).unsqueeze_(0).long()
+        # TODO: choose one return
+        return chan_ids
+        # return torch.tensor(chan_ids).unsqueeze_(0).long()
     
     def fix_init_weight(self):
         def rescale(param, layer_id):
