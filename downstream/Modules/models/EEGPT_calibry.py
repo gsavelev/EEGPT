1f95be0d8f2e1c8c3be7a189f325510b9f044bcf
gsavelev <super.gleb@pm.me>
2025-05-08 14:58:35 +0300

    align img_size to target_timepoints

--- downstream/Modules/models/EEGPT_calibry.py
+++ downstream/Modules/models/EEGPT_calibry.py
@@ -42,7 +42,7 @@ class EEGPTCalibry(pl.LightningModule):
         self.save_hyperparameters()
         
         self.target_encoder = EEGTransformer(
-            img_size=[self.chans_num, int(256*30)],
+            img_size=[self.chans_num, 3000],
             patch_size=32*2,
             embed_num=4,
             embed_dim=512,
