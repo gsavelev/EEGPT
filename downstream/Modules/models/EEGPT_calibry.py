60ddc90a2046afd63f7b1418d93fff5a7d24fffe
gsavelev <super.gleb@pm.me>
2025-05-08 16:12:16 +0300

    change img_size and patch_size to match data shape

--- downstream/Modules/models/EEGPT_calibry.py
+++ downstream/Modules/models/EEGPT_calibry.py
@@ -42,8 +42,8 @@ class EEGPTCalibry(pl.LightningModule):
         self.save_hyperparameters()
         
         self.target_encoder = EEGTransformer(
-            img_size=[self.chans_num, 2944],
-            patch_size=32*2,
+            img_size=[self.chans_num, 60*50],
+            patch_size=60,
             embed_num=4,
             embed_dim=512,
             depth=8,
